cfca9d1ce91ec1aa60870c9ed5f2b67cd3bfb5b7
lukasz-kukulka <qqla83@gmail.com>
2021-02-12 19:06:26 +0000

    Palindrom optimalization

--- palindor.cpp
+++ palindor.cpp
@@ -8,16 +8,16 @@ int main() {
     std::string text;
     std::cin >> text;
     if (isPalindrome(text) == true){
-        std::cout<< "Tekst jest palidromem" << std::endl;
+        std::cout<< "Tekst jest palidromem." << std::endl;
     }
     else {
-        std::cout<< "Tekst NIE jest palidromem" << std::endl;
+        std::cout<< "Tekst NIE jest palidromem." << std::endl;
     }
     return 0;
 }
 
 bool isPalindrome(const std::string & text) {
-    for (uint8_t i = 0, j = text.size() - 1; i < text.size(); i++, j--){
+    for (uint8_t i = 0, j = text.size() - 1; i <= j; i++, j--){
         if (text[i] != text[j]){
             return false;
         }
